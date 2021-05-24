f8267d46511e7267abefa456e36be1ffc5d8c2c5
Hes097 <henning097@live.com>
2021-05-24 15:58:51 +0200

    Update HTML Anfragemethoden
    Update der HTML Anfragemethoden für die  Datenbank,  Like-Dislike Funktion und den Login.
    
    Co-Authored-By: BackyTabba <48299231+BackyTabba@users.noreply.github.com>

--- server.js
+++ server.js
@@ -7,8 +7,56 @@ var server = app.listen(3000, () => console.log("listening on port " + 3000 + "!
 
 //https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
 app.use(express.json());
+
+
 app.use("/html",express.static(__dirname+"/src/html/"));
 
+//Website
 app.get("/" ,(req,res)=>{
     res.sendFile(__dirname+"/src/html/index.html")
     })
+
+
+
+//Entwurfs-Datenbank: get; post:dbabfrage mit parametern; update: parameter auswahl, param change; delete: params
+app.get("/db",(req,res)=>{ 
+    //async öffnen des DBServer
+    next();});
+app.post("/db",(req,res)=>{ 
+    //async öffnen des DBServer
+    next();});
+app.update("/db",(req,res)=>{ 
+    //async öffnen des DBServer
+    next();
+});
+app.delete("/db",(req,res)=>{ 
+    //async öffnen des DBServer
+    next();
+});
+
+
+//Like-Dislike Funktion: get; post: dbabfrage mit parametern; update parameter; 
+app.get("/likes",(req,res)=>{
+    //async öffnen des DBServer/likes
+    next(); 
+})
+app.post("/likes",(req, res)=>{
+    //async öffnen des DBServer/likes
+    next(); 
+})
+app.update("/likes", (req, res)=>{
+    //async öffnen des DBServer/likes
+    next(); 
+})
+
+
+//Login: get; post: dbabfrage mit login informationen; update: login; param change; 
+app.post("/path",(req, res)=>{ next(); 
+}); 
+app.delete("/logout",(req,res)=>{
+    req.logOut()
+    res.redirect("/login")
+})
+
+
+
