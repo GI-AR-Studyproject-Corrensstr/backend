b8c36d14000a406a1008669d2f7c25e2d31a02dd
JudithBre <61976163+JudithBre@users.noreply.github.com>
2021-06-14 14:23:02 +0200

    Creating the functions with JS for the buttons in the HTML

---
+++ src/html/public/script.js
@@ -0,0 +1,59 @@
+//**various jshint configs**
+// jshint esversion: 8
+// jshint browser: true
+// jshint node: true
+// jshint -W117
+// jshint -W083
+"use strict";
+
+// Funktionen fuer die Button auf der Seite index
+function login(){
+  /* */
+}
+function weiterZurRegistrierung(){
+  /* */
+}
+
+// Funktionen fuer die Button auf der Seite index_Registrierung
+function createUser(){
+  /* */
+}
+function deleteInputs(){
+  /* */
+}
+
+// Funktionen fuer die Button auf der Seite index_user_EntwurfHochladen
+function uploadDraft(){
+  /* */
+}
+function deleteDraft(){
+  /* */
+}
+
+// Funktionen fuer die Button auf der Seite index_user_EntwurfAnsehen
+/* ----------------- Entwuerfe----------------------*/
+function likekDraft(){ // Enwurf liken
+  /* */
+}
+function dislikeDraft(){  // Entwurf disliken
+  /* */
+}
+function reportDraft(){ // Enwurf melden
+  /* */
+}
+/* ----------------- Kommentare ----------------------*/
+function addDraft(){  // Kommentar hinzufuegen
+  /* */
+}
+function editDraft(){  // Kommentar bearbeiten
+  /* */
+}
+function likeDraft(){  // Kommentar liken
+  /* */
+}
+function dislikeComment(){  // Kommentar disliken
+  /* */
+}
+function reportComment(){  // Kommentar melden
+  /* */
+}
