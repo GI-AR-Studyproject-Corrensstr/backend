af472410773559d6245b77a15f7b3e82aad025ce
BackyTabba <48299231+BackyTabba@users.noreply.github.com>
2021-06-28 17:00:41 +0200

    Update index.js

--- testservermaster/index.js
+++ testservermaster/index.js
@@ -14,3 +14,77 @@ app.post("/" ,(req,res,next)=>{
     res.send(req.body);
     //res.send("[POST Masterserver:Es klappt!]");
     })
+app.post("/register", (req,res)=>{
+    answereobject={
+        "data": {
+          "id": 55,
+          "first_name": "Max",
+          "last_name": "Mustermann",
+          "profile_photo": null,
+          "role": "citizen"
+        }
+      }
+    res.send(answereobject);
+})
+
+app.post("/login", (req,res)=>{
+    answereobject={
+        "data": {
+          "id": 55,
+          "first_name": "Max",
+          "last_name": "Mustermann",
+          "profile_photo": null,
+          "role": "citizen"
+        }
+      }
+    res.send(answereobject);
+})
+// localhost/Project/ID/
+app.get("/Project", (req,res)=>{
+    answereobject={
+        "data": {
+          "id": 1,
+          "created_at": "2021-06-11T23:11:55.000000Z",
+          "updated_at": "2021-06-11T23:11:55.000000Z",
+          "name": "Qui.",
+          "description": "Vel minima.",
+          "content": "Sed amet cum quo.",
+          "visible": 0,
+          "is_event": 1,
+          "start_time": "2021-07-06 16:55:26",
+          "end_time": "2021-09-12 07:40:04"
+        }
+      }
+      res.send(answereobject);
+})
+app.post("/Project", (req,res)=>{
+    answereobject={
+        "name": "Neues Projekt",
+        "description": "Dies ist ein Projekt, das neu hinzugekommen ist.",
+        "start_time": "2016-06-09",
+        "end_time": "2016-06-28",
+        "visible": "1",
+        "is_event": "0",
+        "updated_at": "2021-06-16T19:03:01.000000Z",
+        "created_at": "2021-06-16T19:03:01.000000Z",
+        "id": 7
+      }
+      res.send(answereobject);
+});
+
+app.put("/Project", (req,res)=>{
+    answereobject={
+    "id": 1,
+    "created_at": "2021-06-11T23:11:55.000000Z",
+    "updated_at": "2021-06-16T19:06:34.000000Z",
+    "name": "Neues verändertes Projekt",
+    "description": "Diese Projekt wurde geändert",
+    "content": "Sed amet cum quo.",
+    "visible": 0,
+    "is_event": 0,
+    "start_time": "2021-07-06 16:55:26",
+    "end_time": "2021-09-12 07:40:04"
+  }
+  res.send(answereobject);
+}
+)
